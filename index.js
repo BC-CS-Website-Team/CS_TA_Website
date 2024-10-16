02fc9eefcd8bb7531c71225cfb2de593d572ac36
ramazanim <ramazanim@berea.edu>
2024-10-16 02:05:05 -0400

    refactored codebase

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public')); 
+
+app.get('/', (req, res) => {
+  res.render('home', {name: 'Ali Ramazani'});
+});
+
+app.get('/creativespace', (req, res) =>{
+  res.render('creativespace', {name: 'Ali Ramazani'});
+});
+
+app.get('/diversity', (req, res) => {
+  res.render('diversity', {name: 'Ali Ramazani'});
+});
+
+app.get('/meet-tas', (req, res) => {
+  res.render('meet-tas');  
+})
+
+app.get('/clubs', (req, res) => {
+  res.render('clubs');
+})
+
+app.get('/career-development', (req, res) => {
+  res.render('career-development');
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
