12d0758e55617d5bebc0eee7f6e79535afe55c9c
Nicholas Hamilton <hamiltonn@berea.edu>
2025-01-22 12:07:07 -0500

    refactored faculty data to designated faculty data page

---
+++ frontend/src/data/facultyData.js
@@ -0,0 +1,82 @@
+/**
+ * facultyData.js
+ * Contains data for faculty members in the Computer Science department
+ */
+
+export const facultyData = [
+  {
+    id: 'Scott-Heggen',
+    name: 'Dr. Scott Heggen',
+    image: '/assets/images/drHeggen1.jpeg',
+    email: 'heggens@berea.edu',
+    /* Temporarily commented out until verified
+    hours: [
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }]
+    ],
+    courses: [
+      ["M/W/F", "CSC226(A): Software Design and Implementation", "9:20-10:30"],
+      ["M/W/F", "CSC226(B): Software Design and Implementation", "10:40-11:50"]
+    ],
+    */
+    links: [
+      ["Slack", "https://bereacs.slack.com/team/U3TH18WAC"],
+      ["Berea Profile", "https://www.berea.edu/academics/departments-programs/computer-science/faculty-staff/dr-scott-heggen"]
+    ]
+  },
+  {
+    id: 'Jan-Pearce',
+    name: 'Dr. Jan Pearce',
+    image: '/assets/images/drPearce1.jpg',
+    email: 'pearcej@berea.edu',
+    /* Temporarily commented out until verified
+    hours: [
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }],
+      [{ start: 13, end: 14 }]
+    ],
+    courses: [
+      ["M/W/F", "CSC236: Data Structures", "9:20-10:30"],
+      ["T/Th", "CSC124: Introduction to Programming", "10:00-11:50"]
+    ],
+    */
+    links: [
+      ["Slack", "https://bereacs.slack.com/team/UXXXXXXXX"],
+      ["Berea Profile", "https://www.berea.edu/academics/departments-programs/computer-science/faculty-staff/dr-jan-pearce"]
+    ]
+  },
+  {
+    id: 'Mario-Nakazawa',
+    name: 'Dr. Mario Nakazawa',
+    image: '/assets/images/drNakazawa1.jpg'
+  },
+  {
+    id: 'Jasmine-Jones',
+    name: 'Dr. Jasmine Jones',
+    image: '/assets/images/drJones1.jpg'
+  },
+  {
+    id: 'Jacqueline-Boggs',
+    name: 'Dr. Jacqueline "Jacci" Boggs',
+    image: '/assets/images/drBoggs1.png'
+  },
+  {
+    id: 'Deanna-Wilborne',
+    name: 'Prof. Deanna Wilborne',
+    image: '/assets/images/profWilborne1.jpeg'
+  },
+  {
+    id: 'Brian-Ramsey',
+    name: 'Brian Ramsey',
+    image: '/assets/images/brian1.png'
+  }
+]
